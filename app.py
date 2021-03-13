58dc2be514d529e50688c91b4bd8d0625247f8a3
Gene Olafsen <engineering@strattonassociates.com>
2021-03-13 23:48:44 +0000

    changes

--- app.py
+++ app.py
@@ -3,9 +3,63 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
+import argparse
+import os
+from flask import Flask, jsonify, make_response
+from flask_swagger_ui import get_swaggerui_blueprint
+from routes import request_api
+
 app = Flask(__name__)
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static',path)
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Recommender-System-Python-Flask-REST"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
+# 
+app.register_blueprint(request_api.get_blueprint())
+
+@app.errorhandler(400)
+def handle_400_error(_error):
+    """Return a http 400 error to client"""
+    return make_response(jsonify({'error': 'Misunderstood'}), 400)
+
+
+@app.errorhandler(401)
+def handle_401_error(_error):
+    """Return a http 401 error to client"""
+    return make_response(jsonify({'error': 'Unauthorised'}), 401)
+
+
+@app.errorhandler(404)
+def handle_404_error(_error):
+    """Return a http 404 error to client"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(500)
+def handle_500_error(_error):
+    """Return a http 500 error to client"""
+    return make_response(jsonify({'error': 'Server error'}), 500)
+
 
 @app.route("/")
 def hello():
+    print("hello world")
     return app.send_static_file("index.html")
+
+@app.route('/hello')
+def hello_world():
+   return "hello world"
