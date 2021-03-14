c2d40c7d5ac9673e0f52735b4d8fa9a7834161f6
Gene Olafsen <engineering@strattonassociates.com>
2021-03-14 15:40:02 +0000

    error handlers

--- app.py
+++ app.py
@@ -27,6 +27,42 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 ### end swagger specific ###
 
+app.register_blueprint(request_api.get_blueprint())
+
+@app.errorhandler(400)
+def handle_400_error(_error):
+    """Return a http 400 error to client"""
+    return make_response(jsonify({'error': 'Misunderstood'}), 400)
+
+
+@app.errorhandler(401)
+def handle_401_error(_error):
+    """Return a http 401 error to client"""
+    return make_response(jsonify({'error': 'Unauthorised'}), 401)
+
+
+@app.errorhandler(404)
+def handle_404_error(_error):
+    """Return a http 404 error to client"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(500)
+def handle_500_error(_error):
+    """Return a http 500 error to client"""
+    return make_response(jsonify({'error': 'Server error'}), 500)
+
+
+@app.route("/")
+def hello():
+    print("hello world")
+    return app.send_static_file("index.html")
+
+@app.route('/hello')
+def hello_world():
+   return "hello world"
+
+
 # index route
 # params
 @app.route("/")
