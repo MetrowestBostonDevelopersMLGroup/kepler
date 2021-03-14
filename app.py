7890ad619bab22373d8d35c10435682342abb6d7
Gene Olafsen <engineering@strattonassociates.com>
2021-03-14 15:27:35 +0000

    add routes and swagger

--- app.py
+++ app.py
@@ -2,7 +2,30 @@ from flask import Flask
 from flask import render_template
 import json
 
+import argparse
+import os
+from flask import jsonify, make_response
+from flask_swagger_ui import get_swaggerui_blueprint
+from routes import request_api
+
 app = Flask(__name__)
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static',path)
+
+## swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Recommender-System-Python-Flask-REST"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
 
 # index route
 # params
